ce38717a70bdd946c1c44d39a9e7b50ab65afa00
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-02-20 10:23:00 +0100

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -82,7 +82,11 @@ def save_and_upload_to_github(data):
     elif response.status_code == 201:  # 201 = creato
         st.success("File creato con successo su GitHub!")
     else:
-        st.error(f"Errore durante l'upload: {response.status_code}\n{response.json()}")
+        #st.error(f"Errore durante l'upload: {response.status_code}\n{response.json()}")
+        # Attendi un tempo casuale tra 0 e 5 secondi prima di riprovare
+        wait_time = random.uniform(0, 5)
+        time.sleep(wait_time)
+        save_and_upload_to_github(data)
         
     st.write(new_df)
     
