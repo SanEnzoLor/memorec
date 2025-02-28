48f4f557b9d164523b0b7a6cd5c29a2aa8b98b67
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-02-28 10:29:59 +0100

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -167,42 +167,42 @@ def BDI2():
 def RRS():
     st.header("**Ruminative Response Scale**")
     st.write("La Scala delle Risposte Ruminative (RRS) è una misura autovalutata progettata per valutare la frequenza con cui gli individui si impegnano in diversi tipi di pensieri e comportamenti ruminativi.")
-    st.write("Gli individui pensano e agiscono in molti modi diversi quando si sentono depressi. Per favore, legga ciascuno dei seguenti item e indichi se, quando si sente giù, triste o depresso, lo pensa o lo fa mai, a volte, spesso o sempre. Indichi cortesemente cosa fa di solito, non cosa pensa di dover fare, selezionando il numero per indicare quanto ogni problema la affligge:")
+    st.write("Gli individui pensano e agiscono in molti modi diversi quando si sentono depressi. Per favore, legga ciascuno dei seguenti item e indichi se, quando si sente giù, triste o depresso, lo pensa o lo fa 'mai', 'a volte', 'spesso' o 'sempre'. Indichi cortesemente cosa fa di solito, non cosa pensa di dover fare, selezionando il numero per indicare quanto ogni problema la affligge:")
 
     # Crea quattro colonne per le informazioni
     cl1, cl2, cl3, cl4 = st.columns([0.28, 0.33, 0.24, 0.19])
     # Mostra le scritte nelle colonne
     with cl1:
-        st.write("Quasi mai = 1") 
+        st.write("Mai = 1") 
     with cl2:
         st.write("A volte = 2")
     with cl3:
         st.write("Spesso = 3")
     with cl4:
-        st.write("Quasi sempre = 4")
+        st.write("Sempre = 4")
             
     items = st.slider("Pensare a quanto ti senti solo", min_value=1, max_value=4, step=1)        
-    items = items + st.slider("Pensare “Non sarò in grado di fare il mio lavoro perché mi sento così male”", min_value=1, max_value=4, step=1)
-    items = items + st.slider("Pensare alle tue sensazioni di fatica e malessere", min_value=1, max_value=4, step=1)
-    items = items + st.slider("Pensare a quanto è difficile concentrarsi", min_value=1, max_value=4, step=1)
+    items = items + st.slider("Pensare “Non sarò in grado di svolgere il mio lavoro se non mi libero di questo”", min_value=1, max_value=4, step=1)
+    items = items + st.slider("Pensare alle tue sensazioni di stanchezza e malessere", min_value=1, max_value=4, step=1)
+    items = items + st.slider("Pensare a quanto sia difficile concentrarti", min_value=1, max_value=4, step=1)
+    items = items + st.slider("Pensare “Cosa ho fatto per meritarmi questo?”", min_value=1, max_value=4, step=1)
     items = items + st.slider("Pensare a quanto ti senti passivo e demotivato", min_value=1, max_value=4, step=1)
-    items = items + st.slider("Analizzare eventi recenti per cercare di comprendere perché sei depresso", min_value=1, max_value=4, step=1)
-    items = items + st.slider("Pensare alla sensazione di non provare più niente", min_value=1, max_value=4, step=1)
+    items = items + st.slider("Analizzare eventi recenti per cercare di capire perché sei depresso", min_value=1, max_value=4, step=1)
+    items = items + st.slider("Pensare al fatto che ti sembra di non sentire più niente", min_value=1, max_value=4, step=1)
     items = items + st.slider("Pensare “Perché non riesco a riprendermi?”", min_value=1, max_value=4, step=1)
     items = items + st.slider("Pensare “Perché reagisco sempre in questo modo?”", min_value=1, max_value=4, step=1)
-    items = items + st.slider("Isolarti e pensare perché ti senti in questo modo", min_value=1, max_value=4, step=1)
-    items = items + st.slider("Scrivere cosa pensi e analizzarlo", min_value=1, max_value=4, step=1)
-    items = items + st.slider("Pensare a una situazione recente e desiderare che fosse andata meglio", min_value=1, max_value=4, step=1)
+    items = items + st.slider("Andare via da solo e pensare al perché ti senti in questo modo", min_value=1, max_value=4, step=1)
+    items = items + st.slider("Scrivere cosa stai pensando e analizzarlo", min_value=1, max_value=4, step=1)
+    items = items + st.slider("Pensare a una situazione recente, desiderando che fosse andata meglio", min_value=1, max_value=4, step=1)
+    items = items + st.slider("Pensare “Non riuscirò a concentrarmi se continuo a sentirmi in questo modo”", min_value=1, max_value=4, step=1)
     items = items + st.slider("Pensare “Perché ho problemi che gli altri non hanno?”", min_value=1, max_value=4, step=1)
+    items = items + st.slider("Pensare “Perché non tratto meglio le cose?”", min_value=1, max_value=4, step=1)
     items = items + st.slider("Pensare a quanto ti senti triste", min_value=1, max_value=4, step=1)
-    items = items + st.slider("Pensare a tutte le tue mancanze, fallimenti, colpe, errori", min_value=1, max_value=4, step=1)
-    items = items + st.slider("Pensare a quanto non hai voglia di fare nulla", min_value=1, max_value=4, step=1)
+    items = items + st.slider("Pensare a tutte le tue mancanze, difetti, colpe, errori", min_value=1, max_value=4, step=1)
+    items = items + st.slider("Pensare al fatto che non te la senti di far nulla", min_value=1, max_value=4, step=1)
     items = items + st.slider("Analizzare la tua personalità per cercare di capire perché sei depresso", min_value=1, max_value=4, step=1)
-    items = items + st.slider("Andare in giro da solo e pensare ai tuoi sentimenti", min_value=1, max_value=4, step=1)
+    items = items + st.slider("Andare da solo in un qualche posto per pensare alle tue emozioni", min_value=1, max_value=4, step=1)
     items = items + st.slider("Pensare a quanto sei arrabbiato con te stesso", min_value=1, max_value=4, step=1)
-    items = items + st.slider("Ascoltare musica triste", min_value=1, max_value=4, step=1)
-    items = items + st.slider("Isolarti e pensare alle ragioni per cui ti senti triste", min_value=1, max_value=4, step=1)
-    items = items + st.slider("Cercare di comprendere te stesso focalizzandoti sui sentimenti depressivi", min_value=1, max_value=4, step=1)
 
     return items
 
