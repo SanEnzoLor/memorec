210fdc4093bc84dfe68ef8b77aa412990ede4e06
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-04-08 12:20:33 +0200

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -308,8 +308,8 @@ def main():
     caregiver = st.selectbox("In questo momento si sta fornendo assistenza a un familiare non autosufficiente (caregiver informale):", ["SI","NO"], index=1)
     autonomia = st.selectbox("Indicare se durante le attività giornaliere si possiede una limitazione all'autonomia:", ["NO", "Fisica", "Mentale"], index=0)
     if autonomia != "NO":
-        desc = st.text_input(f"Se si vuole aggiungere una breve descrizione della propria limitazione {autonomia}:")
-        autonomia = f"Limitazione {autonomia}. {desc}"
+        desc = st.text_input(f"Se si vuole aggiungere una descrizione della propria limitazione {autonomia.lower()}:")
+        autonomia = f"Limitazione {autonomia}. {desc[0].upper() + desc[1:]}"
     
     
     results_d = BDI2()
