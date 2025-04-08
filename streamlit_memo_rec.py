35d13b5812297e4423294aae09c7b3b59b66d48b
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-04-08 20:45:29 +0200

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -340,7 +340,7 @@ def main():
 
     
     
-    """file = st.file_uploader("Se si è interrotta la sessione precedente senza completare il task carica il file scaricato (<nome_file>.csv) al termine della stessa:", type=["csv"])
+    file = st.file_uploader("Se si è interrotta la sessione precedente senza completare il task carica il file scaricato (<nome_file>.csv) al termine della stessa:", type=["csv"])
     if file:
         df_ses_p = pd.read_csv(file)
         #data_all = download_github()
@@ -357,12 +357,11 @@ def main():
             else:
                 cue_words_ref = ['ECCITATO', 'ANNOIATO', 'FELICE', 'FALLITO', 'FORTUNATO', 'DISPERATO', 'RILASSATO', 'SOLITARIO', 'SERENO', 'TRISTE']
             cue_words_p_r = [p for p in cue_words_ref if p not in cue_word_p]
-            st.session_state.remaining_words = cue_words_p_r.copy()
+            if "remaining_words" not in st.session_state:
+                st.session_state.remaining_words = cue_words_p_r.copy()
             st.write(st.session_state.remaining_words)
         #else:
         #    st.error("Il file caricato non corrisponde a nessuno dei dati salvati nella banca dati di GitHub.")
-
-    st.write(st.session_state.remaining_words)"""
 
 
 
@@ -446,39 +445,6 @@ def main():
         st.session_state.testo = ""
     if "ind" not in st.session_state:
         st.session_state.ind = "Inizia"
-
-
-
-    
-
-    file = st.file_uploader("Se si è interrotta la sessione precedente senza completare il task carica il file scaricato (<nome_file>.csv) al termine della stessa:", type=["csv"])
-    if file:
-        df_ses_p = pd.read_csv(file)
-        #data_all = download_github()
-        #st.write(df_ses_p.iloc[0:,2:])
-        #st.write(df_ses_p)
-        #st.write(data_all)
-        #if df_ses_p.iloc[0:,:].reset_index(drop=True).isin(data_all.reset_index(drop=True)).all().all():
-        if 'Cue-Word' not in df_ses_p.columns:
-            st.error("Il file caricato non contiene la colonna 'Cue-Word'.")
-        else:
-            cue_word_p = df_ses_p["Cue-Word"]
-            if df_ses_p["Gender"][0] == "Femminile":
-                cue_words_ref = ['ECCITATA', 'ANNOIATA', 'FELICE', 'FALLITA', 'FORTUNATA', 'DISPERATA', 'RILASSATA', 'SOLITARIA', 'SERENA', 'TRISTE']
-            else:
-                cue_words_ref = ['ECCITATO', 'ANNOIATO', 'FELICE', 'FALLITO', 'FORTUNATO', 'DISPERATO', 'RILASSATO', 'SOLITARIO', 'SERENO', 'TRISTE']
-            cue_words_p_r = [p for p in cue_words_ref if p not in cue_word_p]
-            st.session_state.remaining_words = cue_words_p_r.copy()
-            st.write(st.session_state.remaining_words)
-        #else:
-        #    st.error("Il file caricato non corrisponde a nessuno dei dati salvati nella banca dati di GitHub.")
-
-    st.write(st.session_state.remaining_words)
-
-
-
-
-    
 
     ten_w = False
     
