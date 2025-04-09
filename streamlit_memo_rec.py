314909381eeb26891b9c33a7ced434e857e88cfe
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-04-09 12:02:10 +0200

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -314,7 +314,7 @@ def main():
     if file and st.session_state.file_update == True:
         columns = ["Eta", "Gender", "Nazionalita", "Educazione", "Occupazione", "Caregiver", "Limitazione",  "BDI2", "RRS", "PCL-5-reexperiencing", "PCL-5-avoidance", "PCL-5-altereted_cognition", "PCL-5-hyperarousal", "PCL-5-tot", "Cue-Word"]
         df_ses_p = pd.read_csv(file)
-        if not all(col in df_ses_p.columns for col in columns)::
+        if not all(col in df_ses_p.columns for col in columns):
             st.error("Il file caricato non è corretto.")
         else:
             eta = df_ses_p["Eta"][-1]
