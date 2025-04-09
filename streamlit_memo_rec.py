94cf5e764df7a405192c9c8d688d5ee43fac72d8
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-04-09 16:00:28 +0200

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -389,7 +389,7 @@ def main():
     autonomia_ind = ["NO", "Fisica", "Mentale"].index(st.session_state.autonomia.split('.', 1)[0])
     st.session_state.autonomia = st.selectbox("Indicare se durante le attività giornaliere si possiede una limitazione all'autonomia:", ["NO", "Fisica", "Mentale"], index=autonomia_ind)
     if st.session_state.autonomia != "NO":
-        desc = st.text_input(f"Se si vuole aggiungere una descrizione della propria limitazione {st.session_state.autonomia.lower()}:", value = st.session_state.autonomia.split('.', 1)[1]))
+        desc = st.text_input(f"Se si vuole aggiungere una descrizione della propria limitazione {st.session_state.autonomia.lower()}:", value = st.session_state.autonomia.split('.', 1)[1])
         st.session_state.autonomia = f"{st.session_state.autonomia}. {desc}"
     
     
