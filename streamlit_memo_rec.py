34650df5b8c16a22534acfc9d2949eee21277f93
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-04-10 15:48:39 +0200

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -90,7 +90,7 @@ def save_and_upload_to_github(data):
         time.sleep(wait_time)
         save_and_upload_to_github(data)      
 
-    st.download_button(label="**CLICCA QUI**: Se si volessero **scaricare i dati** di questa sessione, per **completare l'attività successivamente** O per fare **richiesta al gestore dei dati di eliminarli**.", data = new_df.to_csv(index=False))
+    st.download_button(label="**CLICCA QUI**: Se si volessero **scaricare i dati** di questa sessione, per **completare l'attività successivamente** O per fare **richiesta al gestore dei dati di eliminarli**.", data = new_df.to_csv(index=False), icon = "📥", type = "primary")
     
 
 # Funzione per somministrare il BDI2
