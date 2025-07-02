ea9c06ec8330d227c5a0e5fda8316c5ffc191c1e
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-07-02 14:18:37 +0200

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -358,7 +358,7 @@ def main():
     st.write("Questo studio fa parte del progetto di dottorato intitolato:")
     st.write("**Modeling dialogue between human and digital agents for the personalized stimulation of mnemonic abilities and the support for the evaluation of the progress and assistance of neurocognitive problems**")
     st.write("Lo scopo di questo studio è quello di raccogliere memorie autobiografiche eventualmente correlabili a indici noti in letteratura come stimatori dello stato di salute individuale.")
-    st.markdown("I dati raccolti in questo studio, in formato testuale e anonimo (se si registra un file audio, esso sarà cancellato una volta completata la trascrizione automatica), saranno caricati su una banca dati privata di GitHub https://github.com/SanEnzoLor/memo_data.")
+    st.markdown("I dati raccolti in questo studio, in formato testuale e anonimo (se si registra un file audio, esso sarà cancellato una volta completata la trascrizione automatica), saranno caricati su una banca dati temporanea privata, protetta da password, accessibile solo ai ricercatori coinvolti e, solo successivamente alla verifica di completo anonimato, verranno resi pubblici per finalità di ricerca.")
     st.markdown("I dati raccolti da Streamlit Cloud sono anch'essi anonimi e hanno lo scopo di analizzare e gestire l'utilizzo dell'app. In nessun modo sarà possibile risalire all'identità di chi ha completato il test. Per ulteriori informazioni, consulta https://docs.streamlit.io/deploy/streamlit-community-cloud/manage-your-app/app-analytics e https://streamlit.io/privacy-policy.")
     st.markdown("Essendo i dati anonimi, la raccolta è conforme al Regolamento Generale sulla Protezione dei Dati o GDPR.")
     
