601e617f560ae9d3047257dd6b6a313287a1ea81
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-10-27 11:49:17 +0100

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -603,10 +603,12 @@ def main():
             audio_segment.export(temp_file, format="wav")
             st.session_state.transcription = transcribe_audio(temp_file)
 
-        transcription = st.session_state.transcription
         if dispositivo == "Computer":
+            if st.session_state.transcription != "":
+                st.write("**Trascrizione audio:**")
+                st.write(st.session_state.transcription)
             st.session_state.testo = st.text_area("**Scrivi** qui il tuo testo una volta vista la **parola** da cui recuperare la memoria, oppure **modifica** qui la **trascrizione** dell'audio:",
-                                                    value = transcription,
+                                                    value = st.session_state.transcription,
                                                     height = 300,
                                                     key = len(st.session_state.remaining_words),
                                                     disabled = able(st.session_state.show, ten_w),
@@ -614,10 +616,10 @@ def main():
         else:
             if st.session_state.transcription != "":
                 st.write("**Trascrizione audio:**")
-                st.write(transcription)
+                st.write(st.session_state.transcription)
                 st.info("La **modifica** della trascrizione da smartphone potrebbe essere più difficoltosa che da computer, per potervi muovere lungo il testo utilizzare il **cursore mobile** nel campo testuale (tenendo premuto e spostando la lineaa verticale lampeggiante).")
             st.session_state.testo = st.text_input("**Scrivi** qui il tuo testo una volta vista la **parola** da cui recuperare la memoria, oppure **modifica** qui la **trascrizione** dell'audio:",
-                                                    value = transcription,
+                                                    value = st.session_state.transcription,
                                                     key = len(st.session_state.remaining_words),
                                                     disabled = able(st.session_state.show, ten_w),
                                                     label_visibility = visible(st.session_state.show))
@@ -723,6 +725,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
