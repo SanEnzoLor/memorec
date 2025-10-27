be58ca6ef60b2a6c79d63285b0cc58b1008da481
Lorenzo Carozzi <103253178+SanEnzoLor@users.noreply.github.com>
2025-10-27 12:49:52 +0100

    Update streamlit_memo_rec.py

--- streamlit_memo_rec.py
+++ streamlit_memo_rec.py
@@ -601,13 +601,16 @@ def main():
             # Salva temporaneamente il file WAV per la trascrizione
             temp_file = "temp_audio.wav"
             audio_segment.export(temp_file, format="wav")
-            st.session_state.transcription = transcribe_audio(temp_file)
+            transcription = transcribe_audio(temp_file)
+            if trascription != st.session_state.transcription:
+                st.session_state.transcription = transcription
+                st.rerun()
+                
 
         if dispositivo == "Computer":
             if st.session_state.transcription != "":
                 st.write("**Trascrizione audio:**")
                 st.write(st.session_state.transcription)
-                st.rerun()
             st.session_state.testo = st.text_area("**Scrivi** qui il tuo testo una volta vista la **parola** da cui recuperare la memoria, oppure **modifica** qui la **trascrizione** dell'audio:",
                                                     value = st.session_state.transcription,
                                                     height = 300,
@@ -618,7 +621,6 @@ def main():
             if st.session_state.transcription != "":
                 st.write("**Trascrizione audio:**")
                 st.write(st.session_state.transcription)
-                st.rerun()
                 st.info("La **modifica** della trascrizione da smartphone potrebbe essere più difficoltosa che da computer, per potervi muovere lungo il testo utilizzare il **cursore mobile** nel campo testuale (tenendo premuto e spostando la lineaa verticale lampeggiante).")
             st.session_state.testo = st.text_input("**Scrivi** qui il tuo testo una volta vista la **parola** da cui recuperare la memoria, oppure **modifica** qui la **trascrizione** dell'audio:",
                                                     value =  st.session_state.transcription,
@@ -727,6 +729,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
